48b82555eadcc38370eb758d38a184c997e47e77
uebungen <julia.schubatzky@gmail.com>
2024-03-08 16:51:33 +0100

    hue 10

--- hue10.java
+++ hue10.java
@@ -5,9 +5,9 @@ public class hue10 {
 
         for (int i =1; i <=10000; i++) {  //Zahlen von 1 bis 10000 ausgeben lassen
             int sum=0;                      //Summe jeder i Schleife bei 0 starten
-            for (int j =1; j <i ; j++) {    //
-                if (i%j==0){
-                    sum=sum+j;
+            for (int j =1; j <i ; j++) {    //j immer eins kleiner als i, weil nicht durch gleiche Zahl modulo
+                if (i%j==0){                //summe errechnen, wenn i geteilt durch j 0 ergibt
+                    sum=sum+j;              //
 
                 }
             }
